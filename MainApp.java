239057aa24414d7a443348324b1ef1b61a99ac5b
Ivan Neveroff <74492605+Ivan-Ne@users.noreply.github.com>
2022-01-10 14:05:28 +0300

    Add files via upload

---
+++ MainApp.java
@@ -0,0 +1,53 @@
+package hiber;
+
+import com.mysql.cj.xdevapi.SessionFactory;
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+      UserService userService = context.getBean(UserService.class);
+
+      Car car1 = new Car("hg", 99);
+      Car car2 = new Car("ii", 77);
+      User user1 = new User("User5", "LastName5", "user5@mail.ru");
+      User user2 = new User("User6", "LastName6", "user6@mail.ru");
+      user1.setCar(car1);
+      user2.setCar(car2);
+
+      userService.add(user1);
+      userService.add(user2);
+
+//      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
+//      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
+//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
+//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+
+//      List<User> users = userService.listUsers();
+//      for (User user : users) {
+//         System.out.println("Id = "+user.getId());
+//         System.out.println("First Name = "+user.getFirstName());
+//         System.out.println("Last Name = "+user.getLastName());
+//         System.out.println("Email = "+user.getEmail());
+//         System.out.println();
+//      }
+
+      List<User> users = userService.getUserByCar("hg", 99);
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println();
+      }
+
+      context.close();
+   }
+}
